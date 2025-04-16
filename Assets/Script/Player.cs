d9dda2239ef12adecb4ba93d16588504a27f2cbb
louie0523 <louie07230723@gmail.com>
2025-04-16 18:56:39 +0900

    움직임 추가

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float speed = 4f;
+
+    Rigidbody2D rigid;
+    Animator animator;
+
+    private void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+        animator = transform.GetChild(0).GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        rigid.linearVelocity = new Vector2(inputX * speed, rigid.linearVelocity.y);
+
+        if (inputX != 0)
+            Flip(inputX > 0);
+    }
+
+    void Flip(bool facingRight)
+    {
+        Vector3 scale = transform.GetChild(0).localScale;
+        scale.x = facingRight ? -1f : 1f;
+        transform.GetChild(0).localScale = scale;
     }
 }
